eaba20338caba4c140405ecadf7e4fa273689ee8
SekllanChenRuan <xirenchen94@gmail.com>
2020-05-15 13:51:40 -0700

    Added Pics to both files
    Added Pics to the nav1 in  custom_config.php. Added routes for Pic index and view pages.

--- sp20/application/config/custom_config.php
+++ sp20/application/config/custom_config.php
@@ -7,10 +7,12 @@ $config['title'] = 'Default Title';
 $config['copyright'] = 'Default Copyright';
 $config['masthead'] = 'Default MastHead';
 $config['theme'] = 'themes/bootswatch/';
+$config['flickrkey'] = 'da641de256494956cd1ce730188007af';
 
 $config['nav1'] = array(
     'news' => 'News',
-    'news/create' => 'Add News'
+    'news/create' => 'Add News',
+    'pics' => 'Pics'
 );
 
 
--- sp20/application/config/routes.php
+++ sp20/application/config/routes.php
@@ -58,3 +58,10 @@ $route['translate_uri_dashes'] = FALSE;
 $route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
+
+//routes for pics model
+$route['pics/(:any)'] = 'pics/view/$1';
+$route['pics'] = 'pics';
+
+
+
